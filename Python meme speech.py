732de3875953d1466505b859b1ed668286eafb9b
Sanchit <sanchitsingh2301@gmail.com>
2022-08-19 15:47:50 +0530

    removed .py

--- Python meme speech.py
+++
@@ -1,6 +0,0 @@
-user_input=input("What do you hate? ")
-last_letter=user_input[-1::]
-if last_letter=="s":
-    print("If "+ user_input +" have a million haters, I'm one of them. \n If " +user_input+" have just one hater, I'm that one single hater \n If " +user_input+ " have no haters, that means I'm no longer alive. \n If the entire world loves "+user_input+", I hate the entire world. \n Till my last breath, I will hate "+user_input)
-else:
-    print("If "+ user_input +" has a million haters, I'm one of them. \n If " +user_input+" has just one hater, I'm that one single hater \n If " +user_input+ " has no haters, that means I'm no longer alive. \n If the entire world loves "+user_input+", I hate the entire world. \n Till my last breath, I will hate "+user_input)
