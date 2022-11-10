61e43076d9075a6794bd5d37aa76c39483bf230c
Aadikgamer17 <88035442+Aadikgamer17@users.noreply.github.com>
2022-11-10 21:23:41 +0530

    Update js meme speech.js
    Fixed an error in the if statement

--- js meme speech.js
+++ js meme speech.js
@@ -4,7 +4,7 @@ let user_input;
 user_input=document.getElementById("input").value;
 last_letter = user_input.slice(-1);
 
-if (last_letter === "s") {
+if (last_letter == "s") {
   console.log("If " + user_input + " have a million haters, I'm one of them. \n If " + user_input + " have just one hater, I'm that one single hater \n If " + user_input + " have no haters, that means I'm no longer alive. \n If the entire world loves " + user_input + ", I hate the entire world. \n Till my last breath, I will hate " + user_input);
 } else {
   console.log("If " + user_input + " has a million haters, I'm one of them. \n If " + user_input + " has just one hater, I'm that one single hater \n If " + user_input + " has no haters, that means I'm no longer alive. \n If the entire world loves " + user_input + ", I hate the entire world. \n Till my last breath, I will hate " + user_input);
